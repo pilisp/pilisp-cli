a984d2e0808f56d923f0a67b60734f96927bcb8a
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2023-03-30 23:16:13 -0400

    Add loadFile test

--- test/pilisp_cli_test.dart
+++ test/pilisp_cli_test.dart
@@ -1,10 +1,12 @@
+import 'package:pilisp/pilisp.dart';
 import 'package:pilisp_cli/pilisp_cli.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('Test', () {
-    test('Indeed', () {
-      expect(1, 1);
+  group('pilisp_cli', () {
+    test('loadFile', () async {
+      await loadFile(piLispEnv, 'test/pilisp_cli_test.pil', []);
+      expect(PiLisp.loadString('test-answer'), 42);
     });
   });
 }
